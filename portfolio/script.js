f2c89390c530f789105caff0fafd27564b19ca5f
Giovanni Santos <166954268+Giovanni1Santos@users.noreply.github.com>
2025-02-24 22:56:03 -0300

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,47 @@
+const button = document.querySelector("button");
+const modal = document.querySelector("dialog");
+const buttonClose = document.querySelector("dialog button");
+
+button.onclick = function(){
+  modal.showModal();
+}
+
+buttonClose.onclick = function(){
+  modal.close();
+}
+
+function getProjects(){
+    const urlGitHub = 'https://api.github.com/users/Giovanni1Santos/repos'
+    var loadingElement = document.getElementById('loading')
+
+    fetch(urlGitHub,{
+        method: 'GET',
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response)
+            showProjects(response)
+            loadingElement.style.display = 'none'
+        })
+        .catch((e) => {
+            console.log(`Error -> ${e}`)
+        })
+}
+
+function showProjects(data){
+    var listElement = document.getElementById('my-projects-list')
+    for(let i = 0; i < data.length; i++)
+    {
+        let div = document.createElement("div")
+        let a = document.createElement("a")
+        a.href = data[i]['clone_url']
+        a.target = '_blank'
+        a.title = data[i]['description']
+        let linkText = document.createTextNode(data[i]['name']);
+        a.appendChild(linkText);
+        div.appendChild(a)
+        listElement.appendChild(div)
+    }
+}
+
+getProjects()
